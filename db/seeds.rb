6b7f8be28acd06b4156df74a84900d11741cb613
mrAdamLee <adamangel@southern.edu>
2023-03-07 14:59:47 +0100

    navbar added, padding added to index page to not have content hidden behind navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+80.times do
+  Article.create!({title: Faker::Educator.course_name, description: Faker::Hipster.sentences.sample })
+end
